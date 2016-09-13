f3291874bce4032ad401676ed4b354f54c51f622
Ayrton Gomes <com.ayrton@gmail.com>
2016-09-12 21:17:00 -0500

    Added the python stack tutorial

---
+++ nginx-uwsgi-web2py/README.md
@@ -0,0 +1,17 @@
+# NGinx <-> uWSGI <-> web2py
+
+This little guide aims to show you how to setup nginx and uwsgi to work
+
+with web2py applications.
+
+It so simple that it deserve a special place
+
+
+## Begin
+
+        apt-get install nginx-full
+
+        pip install uwsgi
+
+        apt-get install uwsgi-plugin-python
+
---
+++ nginx-uwsgi-web2py/uwsgi.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+
+### BEGIN INIT INFO
+# Provides:          uwsgi
+# Required-Start:    $all
+# Required-Stop:     $all
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: starts the uwsgi app server
+# Description:       starts uwsgi app server using start-stop-daemon
+### END INIT INFO
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/bin
+DAEMON=/usr/local/bin/uwsgi
+OWNER=ayrton
+NAME=uwsgi
+DESC=uwsgi
+UWSGI_LOG_FILE=/home/ayrton/www/logs/uwsgi.log
+WEB2PY_LOCATION=/home/ayrton/www/web2py
+SPOOLER_FILE=/home/ayrton/www/web2py/applications/emprego/modules/spooler.py
+SPOOLER_LOCATION=/home/ayrton/www/web2py/applications/emprego/spooler
+
+test -x $DAEMON || exit 0
+# Include uwsgi defaults if available
+
+if [ -f /etc/default/uwsgi ] ; then
+. /etc/default/uwsgi
+fi
+set -e
+
+#CONFS: http://uwsgi-docs.readthedocs.org/en/latest/Options.html
+DAEMON_OPTS="--socket /tmp/uwsgi.sock --thunder-lock -M -t 30 -A 2 -p 4 -b 65535 -d $UWSGI_LOG_FILE --pythonpath $WEB2PY_LOCATION --module wsgihandler --http-timeout 600 --harakiri 600 --harakiri-verbose --spooler $SPOOLER_LOCATION --import $SPOOLER_FILE"
+#DAEMON_OPTS="--socket /tmp/uwsgi.sock --chmod-socket=666 --thunder-lock -M -t 30 -A 2 -p 4 -b 65535 -d $UWSGI_LOG_FILE --pythonpath $WEB2PY_LOCATION --module wsgihandler --http-timeout 600 --harakiri 600 --harakiri-verbose "
+
+case "$1" in
+        start)
+                echo -n "Starting $DESC: "
+                start-stop-daemon --start --chuid $OWNER:$OWNER --user $OWNER \
+                        --exec $DAEMON -- $DAEMON_OPTS
+                echo "$NAME."
+                ;;
+        stop)
+                echo -n "Stopping $DESC: "
+                start-stop-daemon --signal 3 --user $OWNER --quiet --retry 2 --stop \
+                        --exec $DAEMON
+                echo "$NAME."
+                ;;
+        reload)
+                killall -1 $DAEMON
+                ;;
+        force-reload)
+                killall -15 $DAEMON
+                ;;
+        restart)
+                echo -n "Restarting $DESC: "
+                start-stop-daemon --signal 3 --user $OWNER --quiet --retry 2 --stop \
+                        --exec $DAEMON
+                sleep 1
+                start-stop-daemon --user $OWNER --start --quiet --chuid $OWNER:$OWNER \
+                        --exec $DAEMON -- $DAEMON_OPTS
+                echo "$NAME."
+                ;;
+        status)
+                killall -10 $DAEMON
+                ;;
+        *)
+                N=/etc/init.d/$NAME
+                echo "Usage: $N {start|stop|restart|reload|force-reload|status}" >&2
+                exit 1
+                ;;
+esac
+exit 0
+wrap
+
